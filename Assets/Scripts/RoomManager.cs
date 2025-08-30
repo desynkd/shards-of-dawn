f8b1065408fef39194df59b2c7716c58adc234c6
Ravindu Peeris <ravindupeeris180@gmail.com>
2025-08-30 08:28:16 +0530

    Fix Multiplayer spawning and Add Play game button to redirect to cutscene

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Button startButton;
+
+    void Start()
+    {
+        // Hide the button for non-host players
+        if (PhotonNetwork.IsMasterClient)
+            startButton.gameObject.SetActive(true);
+        else
+            startButton.gameObject.SetActive(false);
+
+        startButton.onClick.AddListener(OnStartGame);
+    }
+
+    private void OnStartGame()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            PhotonNetwork.LoadLevel("Cutscene01");
+        }
+    }
+
+    // In case master client leaves, update button visibility
+    public override void OnMasterClientSwitched(Photon.Realtime.Player newMasterClient)
+    {
+        if (PhotonNetwork.IsMasterClient)
+            startButton.gameObject.SetActive(true);
+        else
+            startButton.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/SpawnPlayers.cs
+++ Assets/Scripts/SpawnPlayers.cs
@@ -1,18 +1,52 @@
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
 
-public class SpawnPlayers : MonoBehaviour
+public class SpawnPlayers : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
+    public float minX, maxX, minY, maxY;
 
-    public float minX;
-    public float maxX;
-    public float minY;
-    public float maxY;
+    private bool hasSpawned = false;
 
-    private void Start()
+    private void OnEnable()
     {
+        Debug.Log("[SpawnPlayers] OnEnable called. PhotonNetwork.InRoom: " + PhotonNetwork.InRoom + ", hasSpawned: " + hasSpawned);
+        if (!hasSpawned)
+        {
+            StartCoroutine(WaitForJoinAndSpawn());
+        }
+    }
+
+    private IEnumerator WaitForJoinAndSpawn()
+    {
+        while (!PhotonNetwork.InRoom || PhotonNetwork.NetworkClientState != ClientState.Joined)
+        {
+            yield return null; // Wait until next frame
+        }
+        if (!hasSpawned)
+        {
+            Debug.Log("[SpawnPlayers] Spawning after join process completed (coroutine).");
+            SpawnMyPlayer();
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("[SpawnPlayers] OnJoinedRoom called. hasSpawned: " + hasSpawned);
+        if (!hasSpawned)
+        {
+            Debug.Log("[SpawnPlayers] Attempting to spawn player from OnJoinedRoom.");
+            SpawnMyPlayer();
+        }
+    }
+
+    private void SpawnMyPlayer()
+    {
+        hasSpawned = true;
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+        Debug.Log("[SpawnPlayers] Spawning player at position: " + randomPosition + " with prefab: " + playerPrefab.name);
         PhotonNetwork.Instantiate(playerPrefab.name, randomPosition, Quaternion.identity);
     }
 }
