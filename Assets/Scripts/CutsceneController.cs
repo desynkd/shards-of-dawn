1e0f7c3eb9260992dbb6bd41426f037ea38a446d
Ravindu Peeris <ravindupeeris180@gmail.com>
2025-08-31 20:52:53 +0530

    Basic Level for one player in Game Level 01

--- Assets/Scripts/CutsceneController.cs
+++ Assets/Scripts/CutsceneController.cs
@@ -13,6 +13,7 @@ public class CutsceneController : MonoBehaviourPunCallbacks
     public string nextSceneName;
 
     private float startTime;
+    private bool cutsceneEnded = false;
 
     void Start()
     {
@@ -20,6 +21,19 @@ public class CutsceneController : MonoBehaviourPunCallbacks
         {
             Debug.Log($"[CutsceneController] MasterClient starting cutscene. Sending RPC_StartCutscene with nextSceneName: {nextSceneName} at network time: {PhotonNetwork.Time}");
             photonView.RPC("RPC_StartCutscene", RpcTarget.AllBuffered, (float)PhotonNetwork.Time, nextSceneName);
+        }
+    }
+
+    void Update()
+    {
+        if (PhotonNetwork.IsMasterClient && !cutsceneEnded)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Debug.Log("[CutsceneController] Enter pressed by MasterClient. Skipping cutscene.");
+                cutsceneEnded = true;
+                photonView.RPC("RPC_SkipCutscene", RpcTarget.AllBuffered, nextSceneName);
+            }
         }
     }
 
@@ -44,6 +58,20 @@ public class CutsceneController : MonoBehaviourPunCallbacks
         {
             Debug.Log($"[CutsceneController] Cutscene finished. MasterClient loading scene: {sceneName}");
             PhotonNetwork.LoadLevel(sceneName);
+        }
+    }
+
+    [PunRPC]
+    void RPC_SkipCutscene(string sceneName)
+    {
+        if (!cutsceneEnded)
+        {
+            cutsceneEnded = true;
+            Debug.Log($"[CutsceneController] RPC_SkipCutscene received. Loading scene: {sceneName}");
+            if (PhotonNetwork.IsMasterClient)
+            {
+                PhotonNetwork.LoadLevel(sceneName);
+            }
         }
     }
 }
